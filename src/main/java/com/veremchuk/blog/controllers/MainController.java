b1cc26645f9e1922da5bea8e157436b562266036
Veremchuk <84232776+Veremchuk@users.noreply.github.com>
2022-03-29 18:45:24 +0200

    In this step was created MainController, HomePage, Model Without MySQL dependencies and jpa

---
+++ src/main/java/com/veremchuk/blog/controllers/MainController.java
@@ -0,0 +1,22 @@
+package com.veremchuk.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Главная страница");
+        return "home";
+    }
+
+    @GetMapping("/about")
+    public String lasttheme(Model model) {
+        model.addAttribute("title", "About");
+        return "about";
+    }
+
+}
