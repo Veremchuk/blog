cc9c971228f47eb5c5a3be7302f29eeff9856959
Veremchuk <84232776+Veremchuk@users.noreply.github.com>
2022-03-30 19:28:11 +0200

    Database spring_web_blog was created and connected to the project

--- src/main/java/com/veremchuk/blog/controllers/BlogController.java
+++ src/main/java/com/veremchuk/blog/controllers/BlogController.java
@@ -1,5 +1,8 @@
 package com.veremchuk.blog.controllers;
 
+import com.veremchuk.blog.models.Post;
+import com.veremchuk.blog.repo.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BlogController {
 
+    @Autowired
+    private PostRepository postRepository;
+
     @GetMapping("/blog")
     public String blogMain(Model model){
-        return "main-blog";
+        Iterable<Post> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
+        return "blog-main";
     }
 }
